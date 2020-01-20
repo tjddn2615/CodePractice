ef46c98a5796217073524b89777b18abb71aa84c
tjddn2615 <tjddn2615@gmail.com>
2020-01-20 20:25:01 +0900

    update sw1206-view.cpp

--- sw1206-view.cpp
+++ sw1206-view.cpp
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <iostream>
 
+using namespace std;
 
 int main(){
+
+	int num;
+	int building[1001]; 
+
+	cin >> num;
+
+	for(i=0;i<10;i++){
+		for(j=0;j<num;j++){
+		
+		}
+	}
 
 
 	return 0;
