871c6227eaa0e45d4b00766bfe3ff00778ede959
swlee <tjddn2615@gmail.com>
2020-02-13 00:35:52 +0900

    update

---
+++ 1245.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+struct magnet {
+	int x;
+	int mass;
+};
+
+int main(){
+
+	int t,i,j,k;
+	int T;
+
+	cin >> T;
+	for(t=1;t<=T;t++){
+		int num;
+		struct magnet m[10];
+		
+		for(int i=0;i<num;i++){
+			cin >> m[i].x >> m[i].mass;
+		}
+
+		
+
+	}	
+
+
+
+
+	return 0;
+}
