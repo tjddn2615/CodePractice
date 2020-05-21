74c5abbbbe6c7401146f968ef870c3d7976e306d
swlee <tjddn2615@gmail.com>
2020-05-21 11:46:00 +0900

    update

---
+++ 1486/1486.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+void dfs(int start, bool check[]){
+		
+
+		
+
+		
+	return;
+}
+
+
+int main(){
+	
+	int T;
+	int tc;
+	int i,j,k;
+
+
+	cin >> T;
+	for(tc=0;tc<T;tc++){
+		
+		vector <int> member;
+		int member_num;
+		int target;
+
+		cin >> member_num;
+		cin >> target;
+
+		for(i=0;i<member_num;i++){
+			int tmp;
+			cin >> tmp;
+			member_num.push_back(tmp);
+		}
+
+		bool check[member_num]
+	
+
+
+	}
+
+		
+
+
+
+	return 0;
+}
---
+++ programmers/43165-dfs/43165.cpp
@@ -0,0 +1,58 @@
+nclude <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int visit[9];
+
+vector<int> input;
+int target_global=0;
+int count=0;
+
+void dfs(int start, int sum){
+		    
+		    int i;
+			    
+		    if( start == (input.size())){
+		       //cout << "sum " << sum << endl;
+			        if(sum == target_global){
+			            count++;
+			        }
+			        return;
+		    }
+		    
+				    
+		    for(i=0; i<2;i++){
+		       //인접 노드
+		        int x = input[start]; 
+		        if(i==0){ 
+		            int tmp = sum+x;
+		            dfs(start+1, tmp);
+		        }else{ 
+		            int tmp = sum-x;
+		            dfs(start+1, tmp);
+		        }								        
+		    }
+					    
+}
+
+
+int solution(vector<int> numbers, int target) {
+	int answer = 0;
+		   
+	int i;
+	for(i=0;i<numbers.size();i++){
+	    input.push_back(numbers[i]);
+	}
+					    
+					    
+    target_global=target;
+						    
+    //cout << "before dfs" << endl;
+    dfs(0,0);
+							    
+    answer=count;
+								    
+    return answer;
+}
