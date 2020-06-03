2e8be0607abb2b34115182059c579896136ff785
Sungwoo <tjddn2615@gmail.com>
2020-06-03 11:35:58 +0900

    update

--- sw1949.cpp
+++ sw1949.cpp
@@ -4,8 +4,35 @@
 
 using namespace std;
 
-int bfs(){
+int dy[4] = {0,1,0,-1};
+int dx[4] = {1,0,-1,0};
 
+int max_level;
+
+
+
+int dfs(int x, int y, int visited[][8], int input[][8], int k, int kflag, int level){
+
+	if(level>max_level){
+		max_level=level;
+	}
+
+	visited[y][x]=1;
+
+	for(int i=0;i<4;i++){
+		int nx=x+dx[i];
+		int ny=y+dy[i];
+
+		if(input[ny][nx] < input[y][x] && input[ny][nx]>0){
+			cout << "next is " << nx << " " << ny << ": " << input[ny][nx] << endl;
+			cout << "level is " << level << endl;
+			dfs(nx,ny,visited,input,k,kflag,level+1);
+		}else if(input[ny][nx]-k < input[y][x] && kflag==0 && input[ny][nx]-k >0){
+			dfs(nx,ny,visited,input,k,1,level+1);
+		}else{
+			continue;
+		}
+	}
 
 
 
@@ -22,13 +49,15 @@ int main(){
 
 	cin >> T;
 
+	cout << T << endl;
 
 	for(i=0;i<T;i++){
 
 		//input
 		int n,k;
 		int v_max=0; 
-		int x_max, y_max;
+		int loc_max[2];//0=y 1=x
+		max_level=0;
 
 		cin >> n;
 		cin >> k;
@@ -39,16 +68,26 @@ int main(){
 				cin >> input[j][k];
 				if(input[j][k]>v_max){
 					v_max=input[j][k];
-					x_max=k;
-					y_max=j;
+					loc_max[0]=j;
+					loc_max[1]=k;
 				}
 			}
 		}
 
 
+		for(j=0;j<n;j++){
+			for(k=0;k<n;k++){
+			cout << input[j][k] << " ";
+			}
+			cout << endl;
+		}
 
 
+		int visited[8][8]= {0};
 
+		dfs(loc_max[1], loc_max[0], visited, input, k, 0, 0);
+
+		cout << "#" << i+1 << " " << max_level+1 << endl;
 	}//test case end
 
 
